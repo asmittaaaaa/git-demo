b3d0fb2652fee89467f7c9c274c388c9bc67b85d
asmittaaaaa <asmitagharat410@gmail.com>
2024-11-23 14:08:42 +0530

    Added Java Project on Git

---
+++ demo-java/src/main/java/first_project/App.java
@@ -0,0 +1,12 @@
+package first_project;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Hey there");
+
+        System.out.println("Hey There!");
+    }
+}
